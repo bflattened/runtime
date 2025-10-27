3e71674cc8fa79fa9458ef2fcd8d4cece2710209
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-10-27 08:43:25 +0100

    Fix x86

--- src/coreclr/nativeaot/zerolib.native/i386/stubs.asm
+++ src/coreclr/nativeaot/zerolib.native/i386/stubs.asm
@@ -8,6 +8,13 @@ Name label proc
 PUBLIC Name
 endm
 
+.const
+align 8
+
+_Int32ToUInt32  DQ  0000000000000000H
+                DQ  41F0000000000000H       ; 2**32
+_DP2to32        EQU (_Int32ToUInt32+8)
+
 .code
 
 PUBLIC RhpByRefAssignRef
@@ -57,5 +64,22 @@ RhpCheckedAssignRefEBP PROC
   mov     DWORD PTR [edx], ebp
   ret
 RhpCheckedAssignRefEBP ENDP
+
+PUBLIC RhpLng2Dbl
+RhpLng2Dbl PROC
+  mov      edx,dword ptr [esp+8]
+  mov      ecx,dword ptr [esp+4]
+  xorps    xmm1,xmm1
+  cvtsi2sd xmm1,edx              ; convert (signed) upper bits
+  xorps    xmm0,xmm0
+  cvtsi2sd xmm0,ecx              ; convert (signed) lower bits
+  shr      ecx,31                ; get sign bit into ecx
+  mulsd    xmm1,_DP2to32         ; adjust upper value for bit position
+  addsd    xmm0,_Int32ToUInt32[ecx*8] ; adjust to unsigned
+  addsd    xmm0,xmm1             ; combine upper and lower
+  movsd    mmword ptr [esp+4],xmm0
+  fld      qword ptr [esp+4]
+  ret
+RhpLng2Dbl ENDP
 
 end
